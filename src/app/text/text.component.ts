b0c92e354ccf78975fe941ee063ac613e3e26bb3
Farhad74 <evgeniy_mgn_74@mail.ru>
2020-04-04 20:06:37 +0500

    create project

---
+++ src/app/text/text.component.ts
@@ -0,0 +1,71 @@
+import {
+  AfterContentChecked,
+  AfterViewInit,
+  Component,
+  QueryList,
+  ElementRef,
+  HostListener,
+  Input,
+  OnInit,
+  Renderer2,
+  ViewChild
+} from '@angular/core';
+import {Text} from '../app.component';
+
+@Component({
+  selector: 'app-text',
+  templateUrl: './text.component.html',
+  styleUrls: ['./text.component.css']
+})
+export class TextComponent implements AfterViewInit {
+
+  @Input() txt: Text;
+  @ViewChild('textArea', {static: false}) areaRef: ElementRef;
+  isShow: boolean = false;
+  isFit:  boolean = false;
+
+  @HostListener('mouseenter') onEnter() {
+    if (!this.isFit) {
+      setTimeout(() => {
+        this.isShow = true;
+      }, 300);
+    }
+  };
+  @HostListener('mouseleave') onleave() {
+    setTimeout(() => {
+      this.isShow = false;
+    }, 400);
+  };
+
+  ngAfterViewInit() {
+    this.display();
+  }
+
+  display() {
+    var box = document.querySelector('.box');
+    var text = box.innerHTML;
+    console.log(text);
+    const line_h = window.getComputedStyle(this.areaRef.nativeElement, null).getPropertyValue('line-height');
+    const realCount = Math.ceil(box.clientHeight / parseInt(line_h, 10));
+    if (realCount > this.txt.countStr) {
+      this.isFit = false;
+      var divTmp = document.createElement('div');
+      divTmp.style.position = 'absolute';
+      divTmp.style.visibility = 'hidden';
+      divTmp.style.width = box.clientWidth + 'px';
+      divTmp.innerHTML = text;
+      divTmp.style.fontSize = line_h;
+      divTmp.style.lineHeight = line_h;
+      document.body.appendChild(divTmp);
+      const new_h = parseInt(line_h, 10) * this.txt.countStr;
+      var i = text.length - 1;
+      for (; i >= 0 && divTmp.clientHeight > new_h; --i) {
+        divTmp.innerHTML = text.substring(0, i) + '...';
+      }
+      box.innerHTML = divTmp.innerHTML;
+    } else {
+      this.isFit = true;
+    }
+  }
+
+}
